48f2a71d4a41ee9a38dcc101e3ed1e09a2021665
Rustam Akbaraliyev <rustam.akbaraliyev@gmail.com>
2023-10-09 20:57:08 +0500

    added commit slash

--- app/src/main/java/uz/xsoft/lessongit/MainActivity.kt
+++ app/src/main/java/uz/xsoft/lessongit/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LessonGitTheme {
+                //
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
